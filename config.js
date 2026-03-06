395b7ab95a2cef2b7ccda2922b9a23f34dc196c5
niuxiangqian <niuxiangqian677@gmail.com>
2026-03-06 16:38:38 +0800

    init

---
+++ config.js
@@ -0,0 +1,1 @@
+const CONFIG={DEFAULT_SERVER_URL:"https://ai-assistant-api.aimegatron.com",APP_VERSION:"1.0.0",APP_NAME:"ChatGPT Batch Question Assistant"};"undefined"!=typeof module&&module.exports&&(module.exports=CONFIG);
